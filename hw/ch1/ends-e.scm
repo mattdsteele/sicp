16844891c0ddb78d81ea3dd4e85afe0876dd095c
Kevin Beam <kwbeam@gmail.com>
2011-02-03 11:22:23 +0800

    Made the ends-e function cleaner

--- hw/ch1/ends-e.scm
+++ hw/ch1/ends-e.scm
@@ -1,4 +1,9 @@
 (define (ends-e s)
   (cond ((equal? s '()) '())
-	((equal? (last (first s)) 'e) (sentence (first s) (ends-e (bf s))))
-	(else (ends-e (bf s)))))
+	((wd-ends-e (first s))
+         (sentence (first s) (ends-e (bf s))))
+	(else
+         (ends-e (bf s)))))
+
+(define (wd-ends-e w)
+  (equal? (last w) 'e))
