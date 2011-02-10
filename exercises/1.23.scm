6726354bcff329483c534335a0c0f9604dd8c1c6
Matt Steele <orphum@gmail.com>
2011-02-09 22:04:09 -0600

    1.23 done

--- exercises/1.23.scm
+++ exercises/1.23.scm
@@ -3,7 +3,10 @@
 (define (find-divisor n test-divisor)
   (cond ((> (square test-divisor) n) n)
         ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (+ test-divisor 1)))))
+        (else (find-divisor n (next test-divisor)))))
 (define (divides? a b)
   (= (remainder b a) 0))
 (define (square x) (* x x))
+(define (next test-divisor)
+  (if (= test-divisor 2) 3
+      (+ test-divisor 2)))
