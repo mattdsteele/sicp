ceaa85a8ab692646102bf6fd99ae0b7334e3a0e6
Matt Steele <orphum@gmail.com>
2011-03-09 23:39:09 -0600

    1.44 done

---
+++ exercises/1.44.scm
@@ -0,0 +1,32 @@
+(define (compose x y)
+  (lambda (z) (x (y z))))
+
+(define (square x) (* x x))
+(define (inc x) (+ x 1))
+
+(define (repeated f n)
+  (define (repeated-iter i result)
+    (if (= i n) result
+        (repeated-iter (+ i 1) (compose f result))))
+  (repeated-iter 1 f))
+
+(define (smooth f)
+  (define dx .1)
+  (define (avg a b c) (/ (+ a b c) 3))
+  (lambda (x)
+    (avg
+        (f (- x dx))
+        (f x)
+        (f (+ x dx)))))
+
+(define my-impulse-function
+  (impulse-maker 0 6))
+
+(define (impulse-maker a y)
+  (lambda (x)
+    (if (= x a)
+        y
+        0)))
+
+(define (n-fold-smooth f n)
+  (repeated (smooth f) n))
