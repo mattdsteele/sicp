61a7735bf31c601ce49f5ffe50a542dac4f7001b
Matt Steele <orphum@gmail.com>
2011-03-31 18:37:55 -0500

    finished 1.12

---
+++ exercises/1.12.scm
@@ -0,0 +1,5 @@
+(define (pascal row column)
+  (cond ((= row 1) 1)
+        ((= row column) 1)
+        ((= column 1) 1)
+        (else (+ (pascal (- row 1) (- column 1)) (pascal (- row 1) column)))))
